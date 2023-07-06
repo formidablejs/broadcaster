891bc86535688feedba1d3ab67797fc69381597c
Donald Pakkies <donaldpakkies@gmail.com>
2023-07-06 22:04:16 +0200

    feat: define channels routes

--- src/server/BroadcastServiceResolver.js
+++ src/server/BroadcastServiceResolver.js
@@ -1,12 +1,45 @@
-const { ServiceResolver } = require('@formidablejs/framework')
+const { Route, ServiceResolver } = require('@formidablejs/framework')
+const send = require('./service')
+const Broadcast = require('./Broadcast')
 
 module.exports = class BroadcastServiceResolver extends ServiceResolver {
     /**
      * Boot broadcast service resolver.
      *
-     * @return {void}
+     * @returns {void}
      */
-    boot() {
-        this.app.register(require('./routes'))
+    register() {
+		const channels = Broadcast.getChannels()
+
+		for (const channel in channels) {
+			Route.get(`/${this.getPrefix()}/${channel}`, (request, reply) => {
+				reply.raw.writeHead(200, {
+					'Content-Type': 'text/event-stream',
+					'Cache-Control': 'no-cache',
+					'Connection': 'keep-alive',
+				})
+
+				send(reply, request, channel)
+			})
+
+			if (channels[channel].name) {
+				Route.name(channels[channel].name)
+			}
+
+			if (channels[channel].middleware) {
+				Route.middleware(channels[channel].middleware)
+			}
+		}
     }
+
+	/**
+	 * Get the prefix for the broadcast routes.
+	 *
+	 * @returns {string}
+	 */
+	getPrefix() {
+		const prefix = this.app.config.get('broadcasting.prefix', '_broadcast')
+
+		return prefix.replace(/\/$/, '').replace(/^\//, '')
+	}
 }
