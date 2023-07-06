badb6de54eaa087ff21a300649b7d3e9734602ed
Donald Pakkies <donaldpakkies@gmail.com>
2023-07-06 22:04:52 +0200

    feat: update exports

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const Broadcast = require('./server/Broadcast')
+const BroadcastServiceResolver = require('./server/BroadcastServiceResolver')
+const Channel = require('./server/Channel')
+
+module.exports = {
+	Broadcast,
+	BroadcastServiceResolver,
+	Channel
+}
--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,9 @@
 const BroadcastServiceResolver = require('./BroadcastServiceResolver')
+const Broadcast = require('./Broadcast')
 const Channel = require('./Channel')
 
 module.exports = {
     BroadcastServiceResolver,
+	Broadcast,
     Channel
 }
