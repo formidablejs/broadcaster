ccd7fc8a70a03013ab7ec00528cab44f82f1528a
Donald Pakkies <donaldpakkies@gmail.com>
2023-07-23 20:57:49 +0200

    feat: add id to each message key

--- src/server/Channel.d.ts
+++ src/server/Channel.d.ts
@@ -1,5 +1,10 @@
 export default class Channel {
     /**
+     * Unique message identifier.
+     */
+    readonly id: string
+
+    /**
      * Create a new channel instance.
      */
     constructor(message: string)
