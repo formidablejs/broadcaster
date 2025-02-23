76b9fc5cdb577e23386ee4f1386ecc675e582256
Donald Pakkies <donaldpakkies@gmail.com>
2025-02-23 18:05:58 +0200

    fix: exports

--- src/client/index.d.ts
+++ src/client/index.d.ts
@@ -1,3 +1,22 @@
+import BroadcastingConfig from '../types/BroadcastingConfig'
 import subscribe from './subscribe'
+import SubscribeCallback from '../../types/SubscribeCallback'
+import SubscribeOnError from '../../types/SubscribeOnError'
+import SubscribeOnReady from '../../types/SubscribeOnReady'
+import SubscriptionOptions from '../../types/SubscriptionOptions'
 
-export { subscribe }
+declare global {
+    interface Window {
+        BroadcastConfig: BroadcastingConfig
+    }
+
+    var BroadcastConfig: BroadcastingConfig
+}
+
+export {
+    subscribe,
+    SubscriptionOptions,
+    SubscribeCallback,
+    SubscribeOnError,
+    SubscribeOnReady
+}
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,6 @@
 import Broadcast from './server/Broadcast'
 import BroadcastChannel from './server/BroadcastChannel'
+import BroadcastingConfig from '../types/BroadcastingConfig'
 import BroadcastServiceResolver from './server/BroadcastServiceResolver'
 import Channel from './server/Channel'
 import ChannelCallback, { ChannelMessage } from './../types/ChannelCallback'
@@ -8,15 +9,6 @@ import SubscribeCallback from './../types/SubscribeCallback'
 import SubscribeOnError from './../types/SubscribeOnError'
 import SubscribeOnReady from './../types/SubscribeOnReady'
 import SubscriptionOptions from './../types/SubscriptionOptions'
-
-type BroadcastingConfig = {
-    /**
-     * Specify the prefix that will be used to prefix the channel
-     * paths. For example, if you specify a prefix of `_broadcast`, the channel
-     * path will be `/_broadcast/channel`.
-     */
-    prefix: string
-}
 
 declare global {
     interface Window {
